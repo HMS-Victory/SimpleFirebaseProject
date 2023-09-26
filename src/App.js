e679386b95f489e7b88e3a17a43c8c4e87a86e0a
HMS-Victory <banrabas.sacket@gmail.com>
2023-09-26 14:13:25 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import {db} from './firebase-config';
+import {collection, getDocs, addDoc, updateDoc, doc, deleteDoc} from 'firebase/firestore';
 
 function App() {
+  const [users, setUsers]=useState([]);
+  const usersCollectionRef=collection(db, "users");
+  const [formData, setFormData]=useState({
+    name: '',
+    age: 0
+  });
+  async function createUser(){
+    await addDoc(usersCollectionRef, formData);
+  }
+
+  useEffect(()=>{
+    const getUsers=async ()=>{
+      const data=await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc)=>({
+        ...doc.data(), id: doc.id
+      })));
+    }
+
+    getUsers();
+  }, [users, usersCollectionRef]);
+
+  function onChangeInput(event){
+    const {id, value}=event.target;
+    setFormData({
+      ...formData,
+      [id]: value
+    });
+    console.log(formData);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input id="name" onChange={onChangeInput} type="text" placeholder="Name..."></input>
+      <input id="age" onChange={onChangeInput} type="number" placeholder="Age"></input>
+      <button onClick={createUser}>Create User</button>
+      {
+        users.map((user)=>{
+          return(
+          <div>
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+            <button onClick={async ()=>{
+              await updateDoc(doc(db, "users", user.id),{age: user.age+1});
+            }}>Increase Age</button>
+            <button onClick={async()=>{
+              await deleteDoc(doc(db, "users", user.id));
+            }}>Delete User</button>
+          </div>);
+        })
+      }
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBySDycyOUbqX9y9q5o13XWBDh7qJs4_Tg",
+  authDomain: "fir-tutorial-64216.firebaseapp.com",
+  projectId: "fir-tutorial-64216",
+  storageBucket: "fir-tutorial-64216.appspot.com",
+  messagingSenderId: "640565087258",
+  appId: "1:640565087258:web:3d58750f05b4c4668c15a3",
+  measurementId: "G-8KG1NM1XFR"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db=getFirestore(app);
