6130d09fbfeba675efb02f25c557983b338a3d7e
HMS-Victory <banrabas.sacket@gmail.com>
2023-09-27 08:50:26 -0600

    first commit

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from '@firebase/firestore';
 
+// I left in the API key because of some errors with dotenv
+// because of the react update to 5.0
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.SENDINGID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASURMENTID
+  apiKey: "AIzaSyBySDycyOUbqX9y9q5o13XWBDh7qJs4_Tg",
+  authDomain: "fir-tutorial-64216.firebaseapp.com", 
+  projectId: "fir-tutorial-64216",
+  storageBucket: "fir-tutorial-64216.appspot.com",
+  messagingSenderId: "640565087258",
+  appId: "1:640565087258:web:3d58750f05b4c4668c15a3",
+  measurementId: "G-8KG1NM1XFR"
 };
 
 
